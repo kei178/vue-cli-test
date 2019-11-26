a2c2425fce71ae4c7fdd1794d4f82d69ee3e6d53
Kei178 <keisuke.inaba.cpa@gmail.com>
2019-11-26 22:46:24 +1100

    Updated sample test

--- __tests__/App.spec.js
+++ __tests__/App.spec.js
@@ -1,9 +1,18 @@
-import { mount } from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
 import Component from '../src/App.vue'
+
+let wrapper
+
+beforeEach(() => {
+  wrapper = shallowMount(Component)
+})
+
+afterEach(() => {
+  wrapper.destroy();
+})
 
 describe('Testing App component', () => {
     it('is a Vue instance', () => {
-      const wrapper = mount(Component)
       expect(wrapper.isVueInstance).toBeTruthy()
     })
 })
